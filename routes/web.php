e44a5fd4eaaf31e218cffd8c85cec4ffa1e382fe
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-08-05 06:35:43 +0530

    d

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
